92c82cf458b82dbc38cd1a26b96d37169d828782
PiGuy1010 <40282600+PiGuy1010@users.noreply.github.com>
2020-11-04 15:38:20 -0500

    In the middle of testing but just wanted to commit something

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import os
+import pickle
 import discord
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -8,4 +12,50 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
+@bot.command(name='rank')
+async def rank(ctx, title, number):
+    #####################################################################
+    # Copied these lines from Google
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    #######################################################################
+    sheetName = title + " (event)"
+    result = sheet.values().get(spreadsheetId = '1xOMJcEq_fVPQ4VXHfuStVNWAC4tw6d5w6i7VDEcPgn4', range=sheetName).execute()
+    values = result.get('values', [])
+    firstSpace = True
+    secondSpace = True
+    for i in range(len(values[0])):
+        if values[0][i] == '1':
+            rankColumn = i
+        if values[0][i] == '':
+            if not firstSpace:
+                if secondSpace:
+                    blankColumn = i
+                    secondSpace = False
+            else:
+                firstSpace = False
+    
 bot.run(TOKEN)
