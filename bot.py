c82e7080eead9e22ca036a855ba0332dee140e1f
PiGuy1010 <40282600+PiGuy1010@users.noreply.github.com>
2020-11-04 16:46:39 -0500

    Is this committing?

--- bot.py
+++ bot.py
@@ -13,49 +13,50 @@ TOKEN = os.getenv('DISCORD_TOKEN') # Sets up the bot with a private token that I
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='rank')
-async def rank(ctx, title, number):
-    #####################################################################
-    # Copied these lines from Google
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+async def time(ctx, title, position):
+    rank = int(position)
+    if rank < 1 or rank > 100:
+        await ctx.send("You entered either a non-positive rank or a rank above 100, which are not allowed.")
+        raise Exception("You entered either a non-positive rank or a rank above 100, which are not allowed.")
+    try:
+        #####################################################################
+        # Copied these lines from Google
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('sheets', 'v4', credentials=creds)
-
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    #######################################################################
-    sheetName = title + " (event)"
-    result = sheet.values().get(spreadsheetId = '1xOMJcEq_fVPQ4VXHfuStVNWAC4tw6d5w6i7VDEcPgn4', range=sheetName).execute()
-    values = result.get('values', [])
-    firstSpace = True
-    secondSpace = True
-    for i in range(len(values[0])):
-        if values[0][i] == '1':
-            rankColumn = i
-        if values[0][i] == '':
-            if not firstSpace:
-                if secondSpace:
-                    blankColumn = i
-                    secondSpace = False
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
             else:
-                firstSpace = False
-    
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        #######################################################################
+        sheetName = title + " (event)"
+        result = sheet.values().get(spreadsheetId = '1xOMJcEq_fVPQ4VXHfuStVNWAC4tw6d5w6i7VDEcPgn4', range=sheetName).execute()
+        values = result.get('values', [])
+        for i in range(3, len(values[0])):
+            if values[0][i] == '':
+                blankColumn = i
+                break
+        rankColumn = 1
+        timeColumn = blankColumn-1
+        print(timeColumn)
+    except:
+        await ctx.send("Some error occurred (most likely typoed or autocorrected title). Please use this command in the form !rank (title) (position).")
 bot.run(TOKEN)
