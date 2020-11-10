b2e8c17a32260a9975475f96d47e2dc46a7c66cb
PiGuy1010 <40282600+PiGuy1010@users.noreply.github.com>
2020-11-09 21:33:47 -0500

    Adding .gitignore

--- bot.py
+++ bot.py
@@ -11,6 +11,31 @@ with open("token.txt") as fin:
     TOKEN = fin.read()
 bot = commands.Bot(command_prefix='!')
 
+#####################################################################
+# Copied these lines from Google
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:                
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+service = build('sheets', 'v4', credentials=creds)
+
 @bot.command(name='eventrank')
 async def time(ctx, title, position):
     rank = int(position)
@@ -18,30 +43,6 @@ async def time(ctx, title, position):
         await ctx.send("You entered either a non-positive rank or a rank above 100, which are not allowed.")
         raise Exception("You entered either a non-positive rank or a rank above 100, which are not allowed.")
     try:
-        #####################################################################
-        # Copied these lines from Google
-        SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
-        creds = None
-        # The file token.pickle stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:                
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
-
-        service = build('sheets', 'v4', credentials=creds)
 
         # Call the Sheets API
         sheet = service.spreadsheets()
