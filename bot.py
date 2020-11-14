40bbce171da9a00b11098b21180ba776e04afbd6
PiGuy1010 <40282600+PiGuy1010@users.noreply.github.com>
2020-11-14 10:22:46 -0500

    Added fastestvid command to bot.py

--- bot.py
+++ bot.py
@@ -166,4 +166,31 @@ async def player(ctx, title, player):
             print(newValues[1][i+2])
             await ctx.send(f"{player} in {title} got an event time of {newValues[1][i+1]}, which was event rank {newValues[1][i+3]}. {player} in {title} also has an overall PB of {newValues[1][i]}, which is rank {newValues[1][i+2]} of known times.")
             break
+@bot.command(name='fastestvid')
+async def findfastvid(ctx,title):
+    try:
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        #######################################################################
+        sheetName = title
+        result = sheet.values().get(spreadsheetId='1xOMJcEq_fVPQ4VXHfuStVNWAC4tw6d5w6i7VDEcPgn4',
+                                    range=sheetName).execute()
+        values = result.get('values', [])
+        resultLink = sheet.values().get(spreadsheetId='1xOMJcEq_fVPQ4VXHfuStVNWAC4tw6d5w6i7VDEcPgn4',
+                                    range=sheetName,fields="sheets/data/rowData/values/hyperlink").execute()
+        valuesLink = resultLink.get('values', [])
+
+        for i in range(len(values[0])):
+            if values[1][i] == "Fastest Time with Video" or values[1][i] == "Fastest Full Run with Video":
+                timeColumn = i
+        for i in range(timeColumn):
+            if valuesLink[2][timeColumn-i] == "":
+                timeColumn -= 1
+            else:
+                break
+
+        await ctx.send(f"fastest video is {values[2][timeColumn]}")
+    except:
+        await ctx.send("some error occured (most likely typoed or autocorrected title). Please use this command in the form !fastestvid (title)")
+
 bot.run(TOKEN)
